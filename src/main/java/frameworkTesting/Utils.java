fa7b2ffdacd50d684a22f75bfed6b9695a7aeac6
Leandro <leandrodavidm@hotmail.com>
2021-11-27 15:21:50 -0300

    Agregado de clse Utils

---
+++ src/main/java/frameworkTesting/Utils.java
@@ -0,0 +1,6 @@
+package frameworkTesting;
+
+public class Utils {
+    final static  String BASE_URL = "https://www.saucedemo.com/";
+    final static  String CHROME_DRIVER_LOCATION = "chromedriver";
+}
